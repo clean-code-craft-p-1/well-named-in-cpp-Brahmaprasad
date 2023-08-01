871a95e671c0ac3349a6ed3e73d18782c676a14b
R <Brahmaprasad.R@philips.com>
2023-08-01 16:11:45 +0530

    Project modularized

---
+++ ColorPair.h
@@ -0,0 +1,21 @@
+class ColorPair {
+private:
+    MajorColor majorColor;
+    MinorColor minorColor;
+public:
+    ColorPair(MajorColor major, MinorColor minor) :
+        majorColor(major), minorColor(minor)
+    {}
+    MajorColor getMajor() {
+        return majorColor;
+    }
+    MinorColor getMinor() {
+        return minorColor;
+    }
+    std::string ToString() {
+        std::string colorPairStr = MajorColorNames[majorColor];
+        colorPairStr += " ";
+        colorPairStr += MinorColorNames[minorColor];
+        return colorPairStr;
+    }
+};
---
+++ TestTelCoColorCoder.h
@@ -0,0 +1,8 @@
+void testPairToNumber(
+    TelCoColorCoder::MajorColor major,
+    TelCoColorCoder::MinorColor minor,
+    int expectedPairNumber);
+
+void testNumberToPair(int pairNumber,
+    TelCoColorCoder::MajorColor expectedMajor,
+    TelCoColorCoder::MinorColor expectedMinor);
