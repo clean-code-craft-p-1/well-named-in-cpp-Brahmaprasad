966ae4ab95945acdb74d54934c52d824d726f0ea
R <Brahmaprasad.R@philips.com>
2023-08-02 10:26:07 +0530

    Fixed issues

--- TestTelCoColorCoder.h
+++
@@ -1,8 +0,0 @@
-void testPairToNumber(
-    TelCoColorCoder::MajorColor major,
-    TelCoColorCoder::MinorColor minor,
-    int expectedPairNumber);
-
-void testNumberToPair(int pairNumber,
-    TelCoColorCoder::MajorColor expectedMajor,
-    TelCoColorCoder::MinorColor expectedMinor);
